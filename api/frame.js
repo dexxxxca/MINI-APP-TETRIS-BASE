7e031629f185f1648ad4eaeb508a5a6d53e3fecf
DEXCA <117394803+dexxxxca@users.noreply.github.com>
2025-10-09 20:22:23 +0200

    Update frame.js

--- api/frame.js
+++ api/frame.js
@@ -1,1 +1,13 @@
+export default function handler(req, res) {
+  const frame = {
+    "frame": {
+      "version": "1",
+      "imageUrl": "https://mini-app-tetris-base-xf5c.vercel.app/preview-thumb.png",
+      "buttonTitle": "🎮 Play Tetris Now",
+      "buttonUrl": "https://mini-app-tetris-base-xf5c.vercel.app",
+      "postUrl": "https://mini-app-tetris-base-xf5c.vercel.app/api/frame"
+    }
+  };
 
+  res.status(200).json(frame);
+}
